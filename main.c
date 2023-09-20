418834b8b58b4b20bbdb507f88d02844c64e794c
Shantito10 <chantelmantentela@gmail.com>
2023-09-20 06:36:39 -0700

    feature main c file

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ * @ac: argument count
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+
 int main(int ac, char **argv)
 {
 	(void)ac;
